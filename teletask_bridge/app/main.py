67128dd6fdc366292730e425633a0d67222f5075
bert-MI4U <148677805+bert-MI4U@users.noreply.github.com>
2026-09-23 18:32:55 +0200

    Update main.py

--- teletask_bridge/app/main.py
+++ teletask_bridge/app/main.py
@@ -59,7 +59,8 @@ async def handle_teletask_event(unit, type, nr, values):
 
     # Also update virtual RGBW lights that use this physical dimmer.
     if key in rgbw_channels:
-        for rgbw_key, channel in rgbw_channelsgroup = rgbw_groups[rgbw_key]
+        for rgbw_key, channel in rgbw_channels[key]
+            group = rgbw_groups[rgbw_key]
 
             # Teletask dimmer feedback is 0..100.
             group[channel] = values[0]
